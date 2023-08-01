043c98f4647098bb1aaabf16ebde26834c943041
Diego Araújo <118080825+DiegoCtrl@users.noreply.github.com>
2023-07-31 23:04:41 -0300

    Add files via upload

---
+++ Tabuada.js
@@ -0,0 +1,8 @@
+numero = parseInt(prompt("De qual númro você quer a tabuada: "))
+
+console.log("Tabuada do ",numero)
+
+for (let index = 1; index <= 10; index++) {
+    resultado = index*numero
+    console.log(`${numero} X ${index} = ${resultado}`) 
+}
