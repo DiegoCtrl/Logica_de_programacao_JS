0690f454fba21177dde686884891ba9b549a5486
Diego Araújo <118080825+DiegoCtrl@users.noreply.github.com>
2023-08-01 10:54:24 -0300

    Add files via upload

---
+++ tipo_var.js
@@ -0,0 +1,12 @@
+let nome
+let idade
+let altura 
+let maior_de_idade
+
+//Atribuindo valores
+nome = "Diego"
+idade = 22
+altura = 1.65
+maior_de_idade = true
+
+print(nome, idade, altura, maior_de_idade)
